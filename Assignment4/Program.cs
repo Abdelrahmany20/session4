ccf548ccfe2d50628cce20be10d67ab73a04b2d9
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2024-12-20 01:28:40 +0200

    Q03

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Assignment4
 {
@@ -31,12 +32,33 @@ namespace Assignment4
 
             //for (int i = 1; i <= 12; i++)
             //{
-                //Console.WriteLine(num*i);
+            //Console.WriteLine(num*i);
             //}
 
 
 
             #endregion
+
+            #region Q03
+            //  Write a program that allows to user to insert number then print all even numbers between 1 to this number
+
+            //Console.Write("Enter an integer: ");
+            //int num =int.Parse(Console.ReadLine());
+
+
+            //for (int i = 1; i <= num; i++)
+            //{
+                
+                //if (i % 2 == 0)
+                   //Console.WriteLine(i);
+                
+            //}
+
+
+
+
+            #endregion
+
 
 
 
