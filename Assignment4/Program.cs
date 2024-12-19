d8c019bad8439e56c5931692370f1ee9324d7fe7
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2024-12-20 01:11:37 +0200

    Q01

---
+++ Assignment4/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+
+namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01
+            //Write a program that allows the user to insert an integer then print all numbers between 1 to that number
+
+            //Console.Write("Enter A Number :");
+            //int number=int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= number; i++)
+            //{
+                //Console.WriteLine(i);
+            //}
+
+
+            #endregion
+
+
+        }
+    }
+}
