3495db205ae7f19862134b8250b184031cb6be26
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2024-12-20 01:22:40 +0200

    Q02

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.ComponentModel;
+using System.Collections.Generic;
 
 namespace Assignment4
 {
@@ -15,11 +16,28 @@ namespace Assignment4
 
             //for (int i = 1; i <= number; i++)
             //{
-                //Console.WriteLine(i);
+            //Console.WriteLine(i);
             //}
 
 
             #endregion
+
+            #region Q02
+            // Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+
+
+            //Console.Write("Enter an integer: ");
+            //int num =int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= 12; i++)
+            //{
+                //Console.WriteLine(num*i);
+            //}
+
+
+
+            #endregion
+
 
 
         }
